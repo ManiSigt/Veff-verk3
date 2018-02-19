a793ab51ef0eb0a0c9b3cd4d50e0b2b9c6e17f40
Hörður Ingi Kristjánsson <hordurk16@ru.is>
2018-02-19 23:00:12 +0000

    Allt sökkar nibba

---
+++ src/components/ListRoom/ListRoom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Poop extends React.Component {
+
+    render(){
+        return 'helo'
+    }
+
+
+};
+
+export default Poop;
---
+++ src/components/Swag/Swag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Swag extends React.Component {
+
+    render(){
+        return 'Yolo'
+    }
+
+
+};
+
+export default Swag;
